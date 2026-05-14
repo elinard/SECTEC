90bafa9e7d5ce8e89e714137fd29285c38695587
morkis-s <goldennascimento@gmail.com>
2026-05-14 20:39:42 -0300

    Email funcionando

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,17 +2,22 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { EmailModule } from '../email/email.module';
+import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
-    ConfigModule, // 👈 adiciona aqui
+    ConfigModule,
+    EmailModule,
+    TypeOrmModule.forFeature([User]),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
